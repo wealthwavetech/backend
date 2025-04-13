08d01680dbc0e09152772e379886774ab6de1f0d
jaydhurve09 <jaydhurve09@gmail.com>
2025-04-13 20:24:42 +0530

    backend config error fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React frontend URL
-    "hhttps://wealthwavetech.vercel.app",
+    "https://wealthwavetech.vercel.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
